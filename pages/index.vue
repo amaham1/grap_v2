2e8021eb0168d257a50a6928c63be5458363c71f
최주영 <akapwhd@gmail.com>
2025-05-23 22:48:17 +0900

    의존성 해결

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <template>
+  <!-- 리디렉션으로 인해 이 내용은 렌더링되지 않습니다. -->
   <div>
-    안녕하세요.
+    페이지를 이동 중입니다...
   </div>
 </template>
 
 <script setup lang="ts">
-
+definePageMeta({
+  redirect: '/alljeju',
+});
 </script>
