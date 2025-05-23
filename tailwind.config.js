ea4d3c37acf920ab72282a8c3b63f5d36768532e
akapwhd <akapwhd@gmail.com>
2025-05-23 07:57:21 +0000

    fix: Tailwind CSS 플러그인 의존성 제거

--- tailwind.config.js
+++ tailwind.config.js
@@ -64,9 +64,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
-  ],
+  plugins: [],
 }
